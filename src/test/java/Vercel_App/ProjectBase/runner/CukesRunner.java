54251cf6d9b38b5dd7570c3dd9684ede8dd93168
cemilbildircin <ccbildircin@gmail.com>
2024-02-23 14:31:32 -0500

    WIP

--- src/test/java/Vercel_App/ProjectBase/runner/CukesRunner.java
+++ src/test/java/Vercel_App/ProjectBase/runner/CukesRunner.java
@@ -14,7 +14,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features/",
         glue = "Vercel_App/ProjectBase/step_definitions",
         dryRun = false,
-        tags = "@dropdown",
+        tags = "@smoke",
         monochrome = true,
         publish = false // online report in cucumber web.
 
